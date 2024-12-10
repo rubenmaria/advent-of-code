53f0055c6dbc2359198cddc9b06811ce759e9831
Lyrexes <steamkonto60@gmail.com>
2024-12-10 17:27:38 +0100

    nine part 1 done

--- 2024/nine.py
+++ 2024/nine.py
@@ -1,71 +1,69 @@
-import copy
-import math
-
-
-memory_line = list(map(int, open("nine.input", "r").read().splitlines()[0]))
-check_sum = 0
-memory_position = 0
-end_memory_line = copy.deepcopy(memory_line)[::-1]
-end_index = 0
-file_id_visited = set()
-for i in range(len(memory_line)):
-    if i % 2 == 0:
-        file_id = int(i / 2)
-        space = memory_line[i]
-
-        if file_id in file_id_visited:
-            space = end_memory_line[end_index]
-            new_add = sum([
-                file_id * pos 
-                for pos in range(memory_position, memory_position + space)
-            ])
-            print(f"last: file_id: {file_id}, space: {space}, sum_added: {new_add}")
-            check_sum += new_add
-            break
-
-        new_add = sum([
-            file_id * pos 
-            for pos in range(memory_position, memory_position + space)
+def calculate_check_sum(memory: list[tuple[int,int]]) -> int:
+    check_sum = 0
+    memory_index = 0
+    for file in memory:
+        (file_id, blocks_used) = file
+        check_sum += sum([
+            pos * file_id 
+            for pos in range(memory_index, memory_index + blocks_used)
         ])
-        print(f"file_id: {file_id}, space: {space}, sum_added: {new_add}")
-        check_sum += new_add
-        memory_position += space
-        file_id_visited.add(file_id)
+        memory_index += blocks_used
+    return check_sum
+
+memory_line = list(map(int, open("dummy-nine.input", "r").read().splitlines()[0]))
+
+files = [(int(i/2), v) for i,v in enumerate(memory_line) if i % 2 == 0]
+free_space = [v for i,v in enumerate(memory_line) if i % 2 == 1]
+deframented_memory: list[tuple[int,int]] = []
+for i in range(len(memory_line)):
+    if len(files) == 0:
+        break
+    if i % 2 == 0:
+        deframented_memory.append(files.pop(0))
     else:
-        free_space = memory_line[i]
-        for pos in range(memory_position, memory_position + free_space):
-            file_id = int((len(memory_line) - 1 - end_index) / 2)
-            file_id_visited.add(file_id)
-            check_sum += file_id * pos
-            print(f"file_id: {file_id}, space: {free_space}, file_added: {file_id*pos}, file_left:{end_memory_line[end_index]}")
-            end_memory_line[end_index] -= 1
-            if end_memory_line[end_index] == 0:
-                end_index += 2
-        memory_position += free_space
-
-#print(memory_line)
-print(len(memory_line))
-print(check_sum)
-
-
+        space = free_space.pop(0)
+        while len(files) > 0 and space > 0:
+            if space >= (files[-1])[1]:
+                done_file = files.pop()
+                space = space - done_file[1]
+                deframented_memory.append(done_file)
+            else:
+                files[-1] = (files[-1][0], files[-1][1] - space)
+                deframented_memory.append((files[-1][0], space))
+                break
+print(files)
+print(free_space)
+print(deframented_memory)
+print(calculate_check_sum(deframented_memory))
 
 
-
-"""
-disk = []; [
-    disk := disk + ([str(int(i/2)) * int(d)] if i % 2 == 0 else ["." * int(d)])
-    for i,d in enumerate(memory_line)
+files = [(int(i/2), v) for i,v in enumerate(memory_line) if i % 2 == 0]
+free_spaces = [v for i,v in enumerate(memory_line) if i % 2 == 1]
+files_and_spaces = [
+    (-1,free_spaces[i//2]) if i % 2 == 1 else files[i//2] 
+    for i in range(len(memory_line))
 ]
-disk = list("".join(disk))
-for i in range(len(disk)):
-    right_of = disk[i+1:]
-    has_number_right = list(map(lambda x: x.isdigit(), right_of))
-    is_number = list(map(lambda x: x.isdigit(), disk))
-    if disk[i] == "." and any(has_number_right):
-        index_last_number = len(disk) - 1 - is_number[::-1].index(True)
-        disk[i], disk[index_last_number] = disk[index_last_number], disk[i]
+print(files_and_spaces)
+for file in files[::-1]:
+    for space_index, freespace in enumerate(files_and_spaces):
+        print(freespace)
+        if free_space[0] != -1:
+            continue
+        space = free_spaces[1]
+        print("space: ", space)
+        if space == file[1]:
+            file_index = files_and_spaces.index(file)
+            files_and_spaces[file_index], files_and_spaces[space_index] = files_and_spaces[space_index], files_and_spaces[file_index]
+            break
+        if space > file[1]:
+            print("Hello")
+            space -= file[1]
+            file_index = files_and_spaces.index(file)
+            left_free_space = (-1, space)
+            files_and_spaces.pop(space_index)
+            files_and_spaces.pop(file_index)
+            files_and_spaces.insert(space_index - 1, file)
+            files_and_spaces.insert(space_index, left_free_space)
 
-disk_numbers = list(map(int, "".join([x if x.isdigit() else "" for x in disk])))
-check_sum = 0; [check_sum := check_sum + x * i for i,x in enumerate(disk_numbers)]
-"""
-
+print(files_and_spaces)
+print(calculate_check_sum(files_and_spaces))
