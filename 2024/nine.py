923c02ba1d0e8211f0cb6f2c485226089798e62f
Lyrexes <steamkonto60@gmail.com>
2024-12-11 11:34:45 +0100

    done with nine and ten and part1 of eleven

--- 2024/nine.py
+++ 2024/nine.py
@@ -6,14 +6,16 @@ def calculate_check_sum(memory: list[tuple[int,int]]) -> int:
         check_sum += sum([
             pos * file_id 
             for pos in range(memory_index, memory_index + blocks_used)
+            if file_id != -1
         ])
         memory_index += blocks_used
     return check_sum
 
-memory_line = list(map(int, open("dummy-nine.input", "r").read().splitlines()[0]))
+
+memory_line = list(map(int, open("nine.input", "r").read().splitlines()[0]))
 
 files = [(int(i/2), v) for i,v in enumerate(memory_line) if i % 2 == 0]
-free_space = [v for i,v in enumerate(memory_line) if i % 2 == 1]
+free_spaces = [v for i,v in enumerate(memory_line) if i % 2 == 1]
 deframented_memory: list[tuple[int,int]] = []
 for i in range(len(memory_line)):
     if len(files) == 0:
@@ -21,7 +23,7 @@ for i in range(len(memory_line)):
     if i % 2 == 0:
         deframented_memory.append(files.pop(0))
     else:
-        space = free_space.pop(0)
+        space = free_spaces.pop(0)
         while len(files) > 0 and space > 0:
             if space >= (files[-1])[1]:
                 done_file = files.pop()
@@ -31,11 +33,7 @@ for i in range(len(memory_line)):
                 files[-1] = (files[-1][0], files[-1][1] - space)
                 deframented_memory.append((files[-1][0], space))
                 break
-print(files)
-print(free_space)
-print(deframented_memory)
 print(calculate_check_sum(deframented_memory))
-
 
 files = [(int(i/2), v) for i,v in enumerate(memory_line) if i % 2 == 0]
 free_spaces = [v for i,v in enumerate(memory_line) if i % 2 == 1]
@@ -43,27 +41,26 @@ files_and_spaces = [
     (-1,free_spaces[i//2]) if i % 2 == 1 else files[i//2] 
     for i in range(len(memory_line))
 ]
-print(files_and_spaces)
 for file in files[::-1]:
-    for space_index, freespace in enumerate(files_and_spaces):
-        print(freespace)
+    f_index = files_and_spaces.index(file)
+    for space_index, free_space in enumerate(files_and_spaces[:f_index]):
         if free_space[0] != -1:
             continue
-        space = free_spaces[1]
-        print("space: ", space)
+        space = free_space[1]
         if space == file[1]:
             file_index = files_and_spaces.index(file)
             files_and_spaces[file_index], files_and_spaces[space_index] = files_and_spaces[space_index], files_and_spaces[file_index]
             break
         if space > file[1]:
-            print("Hello")
             space -= file[1]
-            file_index = files_and_spaces.index(file)
             left_free_space = (-1, space)
+            new_free_space = (-1, file[1])
             files_and_spaces.pop(space_index)
+            file_index = files_and_spaces.index(file)
             files_and_spaces.pop(file_index)
-            files_and_spaces.insert(space_index - 1, file)
-            files_and_spaces.insert(space_index, left_free_space)
+            files_and_spaces.insert(file_index, new_free_space)
+            files_and_spaces.insert(space_index, file)
+            files_and_spaces.insert(space_index+1, left_free_space)
+            break
 
-print(files_and_spaces)
 print(calculate_check_sum(files_and_spaces))
---
+++ 2024/ten.py
@@ -0,0 +1,45 @@
+def trail_head_endings(map: list[list[int]], pos: tuple[int,int])-> set[tuple[int,int]]:
+    if map[pos[0]][pos[1]] == 9:
+        return {pos}
+    positions: set[tuple[int,int]] = set()
+    for dir in [(1,0), (-1,0), (0,1), (0,-1)]:
+        new_pos = (pos[0] + dir[0], pos[1] + dir[1])
+        if (0 <= new_pos[0] < len(map) and 0 <= new_pos[1] < len(map[0])
+                and map[new_pos[0]][new_pos[1]] - map[pos[0]][pos[1]] == 1):
+             positions |= trail_head_endings(map, new_pos)
+    return positions
+
+
+def trail_head_rating(map: list[list[int]], pos: tuple[int,int]) -> int:
+    if map[pos[0]][pos[1]] == 9:
+        return 1
+    rating = 0
+    for dir in [(1,0), (-1,0), (0,1), (0,-1)]:
+        new_pos = (pos[0] + dir[0], pos[1] + dir[1])
+        if (0 <= new_pos[0] < len(map) and 0 <= new_pos[1] < len(map[0])
+             and map[new_pos[0]][new_pos[1]] - map[pos[0]][pos[1]] == 1):
+            rating += trail_head_rating(map, new_pos)
+    return rating
+
+hiking_map_raw = open("ten.input", "r").read().splitlines()
+hiking_map= [[int(x) for x in row]  for row in hiking_map_raw]
+trail_heads_indecies = [
+    (row,column)
+    for row in range(len(hiking_map))
+    for column in range(len(hiking_map[0])) 
+    if hiking_map[row][column] == 0
+]
+score_sum = sum(
+    map(
+        lambda x: len(trail_head_endings(hiking_map, x)),
+        trail_heads_indecies
+    )
+)
+rating_sum = sum(
+    map(
+        lambda x: trail_head_rating(hiking_map, x),
+        trail_heads_indecies
+    )
+)
+print(score_sum)
+print(rating_sum)
